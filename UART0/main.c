c9449acbfd1c0a93672658462ae139afbde69468
DjKrys <krzysiek59999@wp.pl>
2020-01-03 17:31:45 +0100

    UART0 is done

--- UART0/main.c
+++ UART0/main.c
@@ -9,13 +9,6 @@ void ledsInit(void)
 	PTB->PDDR |= 1UL << 9;
 }
 
-void BlinkLED(void){
- uint32_t i = 0;
-
- PTB->PTOR = 1UL << 9;
- for(i = 0; i < 3000000; i++){};
-}
-
 int main (void)
 {
 	ledsInit();
@@ -24,18 +17,18 @@ int main (void)
 	uint8_t data = 0;
 	uint8_t c = 0;
 	
-	//for (c = 1; c <= 10; c++)
-	//{
+	for (c = 'a'; c <= 'g'; c++)
+	{
 		UART0_Transmit(c);
-	//}
+	}
 
 	while (1)
 	{
-		BlinkLED();
-		//data = UART0_Receive();
+
+		data = UART0_Receive();
 		
-		//if (data == 50) PTB->PTOR |= 1UL << 9; // green // kod ASCII
+		if (data == 50) PTB->PTOR |= 1UL << 9; // green // kod ASCII
 		
-		//UART0_Transmit(data);
+		UART0_Transmit(data);
 	}
 }
