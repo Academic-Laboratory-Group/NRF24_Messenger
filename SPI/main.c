3c899d1647e3b25686557f34a18c369af04b2349
DjKrys <krzysiek59999@wp.pl>
2020-01-04 12:51:38 +0100

    Update

--- SPI/main.c
+++ SPI/main.c
@@ -6,7 +6,6 @@ void ledsInit(void)
   PORTB->PCR[9] = PORT_PCR_MUX(1UL);                       
 
 	PTB->PDDR |= 1UL << 9;
-	PTB->PCOR = 1UL << 9;
 }
 
 void BlinkLED(void){
@@ -21,9 +20,10 @@ void spiInit ( void ) {
    
     SIM -> SCGC4 |= SIM_SCGC4_SPI0_MASK ;       // wlacz zegar do modulu spi0
    
-    SIM -> SCGC5 |= SIM_SCGC5_PORTA_MASK ;     // wlacz zegar do portu A.
-		SIM -> SCGC5 |= SIM_SCGC5_PORTB_MASK ;
-	  
+    SIM -> SCGC5 |= (SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK);     // wlacz zegar do portu A.
+	
+		SPI0 -> C1 &=~ SPI_C1_SPE_MASK ;         // Wylacz system SPI
+	
     PORTA -> PCR [7] |= PORT_PCR_MUX (2) ;       // pin7 w porcie A to SPI0 MOSI
 	
 		PORTA -> PCR [6] |= PORT_PCR_MUX (2);					// pin6 w porcie A to SPI0 MISO
@@ -32,35 +32,36 @@ void spiInit ( void ) {
    
     PORTA -> PCR [ 10 ] |= PORT_PCR_MUX (1) ;       // pin10 w porcie A to SS
  
-	  SPI0 -> C1 |= SPI_C1_SPE_MASK ;         // Wlacz system SPI
-   
     SPI0 -> C1 |= SPI_C1_MSTR_MASK ;       // Wybrano tryb glówny
    
     SPI0 -> C1 |= SPI_C1_CPOL_MASK ;       // Biegunowosc zegara - aktywny wysoki  //
    
     SPI0 -> C1 &=~ SPI_C1_CPHA_MASK ;       // srodek transmisji										
+
+    SPI0 -> C1 &=~ SPI_C1_LSBFE_MASK ;     // start transmisji z msb
    
-    //SPI0 -> C1 &=~ SPI_C1_LSBFE_MASK ;     // startsccc transmisji z msb
+    SPI0 -> BR |= SPI_BR_SPPR (0) ;     // predkosci transmisji 
    
-    SPI0 -> BR |= SPI_BR_SPPR ( 0b000 ) ;     // predkosci transmisji ..
-   
-    SPI0 -> BR |= SPI_BR_SPR ( 0b0110 ) ;     // ...
+    SPI0 -> BR |= SPI_BR_SPR (6) ;    
    
     SPI0 -> C2 |= SPI_C2_MODFEN_MASK ;         // slave select									//
    
     SPI0 -> C1 |= SPI_C1_SSOE_MASK ;       // is gpio
 		
-
+		SPI0 -> C1 |= SPI_C1_CPHA_MASK ; 
+		
+	  SPI0 -> C1 |= SPI_C1_SPE_MASK ;					//wlacz SPI
 }
 
 void sendData (int8_t data)
 {
-	while(!(SPI0->S & SPI_S_SPTEF_MASK)) SPI0->D = data;
+	while(!(SPI0->S & SPI_S_SPTEF_MASK));
+	SPI0->D = data;
 }
 
 int main (void)
 {
-	ledsInit();
+	//ledsInit();
 	spiInit();
 
 	while (1)
